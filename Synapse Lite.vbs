a8556268a7d8461e1320b5c97053042805f4c41d
X-Tripen <hubertpaprocki.dev@gmail.com>
2023-07-26 13:15:46 +0200

    Synapse Lite V1.1

--- Synapse Lite.vbs
+++ Synapse Lite.vbs
@@ -1,3 +1,3 @@
 Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run "cmd /c SynapseLite.bat", 0, False
+WshShell.Run "cmd /c SynapseLite.bat", 0, True
 Set WshShell = Nothing
--- SynapseLite.bat
+++ SynapseLite.bat
@@ -3,31 +3,35 @@
 set "max_checks=60"
 set "check_interval=1"
 set "check_count=0"
+set "wait_for_setup=60"
+set "give_time_to_read=3"
 
 :loop
-set "are_running=true"
-for %%p in ("RzActionSvc.exe" "RazerCentralService.exe" "CefSharp.BrowserSubprocess.exe" "GameManagerService.exe") do (
-    set "is_running="
-    for /f "tokens=1" %%i in ('tasklist /nh /fi "imagename eq %%~p" 2^>nul') do (
-        set "is_running=%%i"
-    )
-    if not defined is_running (
-        set "are_running=false"
+set "are_running=false"
+
+for /f "tokens=1" %%i in ('tasklist /nh /fi "imagename eq RazerCentralService.exe"') do (
+    if "%%i"=="RazerCentralService.exe" (
+        set "are_running=true"
+        echo Razer Synapse is running
     )
 )
 
 if "%are_running%"=="true" (
-    Timeout 60
+    Timeout %wait_for_setup%
     taskkill /f /t /im "Razer Synapse 3.exe"
-    taskkill /f /t /im  "RzActionSvc.exe"
+    taskkill /f /t /im "RzActionSvc.exe"
     taskkill /f /t /im "Razer Synapse Service Process.exe"
     taskkill /f /t /im "CefSharp.BrowserSubprocess.exe"
+    timeout 4
 ) else (
-    set /a "check_count+=1"
-    if !check_count! lss %max_checks% (
+    set /a "check_count+=%check_interval%"
+    echo Checking if Razer Synapse is running %check_count% / %max_checks%
+    if %check_count% lss %max_checks% (
         timeout /t %check_interval% /nobreak >nul
         goto loop
     ) else (
+        echo Razer Synapse not running after %max_checks% checks. Exiting.
+        timeout %give_time_to_read% >null
         exit /b 1
     )
 )
